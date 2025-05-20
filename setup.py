48ea9a6b85656621a065aaedd773eb0f71043d05
Edward <edward_tl@hotmail.com>
2025-05-20 14:44:06 -0600

    [RESTRUCTURE] Import libraries
    When testing on local, everything was working fine, needs to be testing on remote to be sure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    author = "Edward Toledo Lopez",
+    description = "A traduction of Oddo's RPC into classes.",
+    name = "odoo_apps",
+    version = "0.1.0",
+    packages=find_packages(
+        include=[
+            "requests (>=2.32.3,<3.0.0)",
+            "pandas (>=2.2.3,<3.0.0)",
+            "openpyxl (>=3.1.5,<4.0.0)",
+            "python-dotenv (>=1.1.0,<2.0.0)",
+            "pydantic (>=2.11.4,<3.0.0)",
+            "pytest (>=8.3.5,<9.0.0)",
+            "pytz (>=2025.2,<2026.0)",
+            "flask (>=3.1.1,<4.0.0)"
+        ]
+    )
+)
